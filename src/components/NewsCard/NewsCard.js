20fd852bea3d4ef61e4f4631f7fd283044d4002f
Mark Bryginskii <m.bryginskii@gmail.com>
2021-01-26 23:15:34 +0700

    fix: fix after first review

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -7,31 +7,35 @@ const NewsCard = (props) => {
 
   const currentUser = React.useContext(CurrentUserContext);
 
+  const [isCardTooltipVisible, setIsCardTooltipVisible] = React.useState(false);
+
+  const handleMouseHover = () => {
+    setIsCardTooltipVisible(!isCardTooltipVisible);
+  }
+
   return (
     <li className="news-card">
       <Switch>
         <Route exact path="/">
-          <button className="news-card__button news-card__save-icon" type="button">
-            <div className="news-card__button-tooltip">
-              <p className="news-card__button-tooltip-text">{props.isLoggedIn ? 'Войдите, чтобы сохранять статьи' : 'Убрать из сохранённых'}</p>
-            </div>
-          </button>
+          <div className={`news-card__button-tooltip ${isCardTooltipVisible && 'news-card__button-tooltip_active'}`}>
+            <p className="news-card__button-tooltip-text">{props.isLoggedIn ? 'Войдите, чтобы сохранять статьи' : 'Убрать из сохранённых'}</p>
+          </div>
+          <button className="news-card__button news-card__save-icon" type="button" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}></button>
         </Route>
         <Route exact path="/saved-news">
           <div className="news-card__categoty">
             <p className="news-card__categoty-text">{props.keyword}</p>
           </div>
-          <button className="news-card__button news-card__trash-icon" type="button">
-            <div className="news-card__button-tooltip">
-              <p className="news-card__button-tooltip-text">Убрать из сохранённых</p>
-            </div>
-          </button>
+          <div className={`news-card__button-tooltip ${isCardTooltipVisible && 'news-card__button-tooltip_active'}`}>
+            <p className="news-card__button-tooltip-text">Убрать из сохранённых</p>
+          </div>
+          <button className="news-card__button news-card__trash-icon" type="button" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}></button>
         </Route>
       </Switch>
       <img className="news-card__image" src={props.image} alt={props.title}/>
       <div className="news-card__footer">
         <a href={props.link} className="news-card__link" target="_blank">
-          <time className="news-card__date">{props.date}</time>
+          <p className="news-card__date">{props.date}</p>
           <h3 className="news-card__title">{props.title}</h3>
           <article className="news-card__article">{props.text}</article>
           <p className="news-card__source">{props.source}</p>
--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './NewsCardList.css';
 import NewsCard from "../NewsCard/NewsCard";
+import NewsCardsData from "../../data/NewsCardsData.json";
 
 const NewsCardList = () => {
 
   return (
     <ul className="news-cards">
-      <NewsCard />
-      <NewsCard />
-      <NewsCard />
-      <NewsCard />
+      {NewsCardsData.map(card => <NewsCard {...card}/>)}
     </ul>);
 }
 
